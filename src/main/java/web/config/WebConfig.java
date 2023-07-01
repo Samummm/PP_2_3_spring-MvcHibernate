13807a3e1527b2eb4e98484644c4c1e38c6bad1c
Samummm <mogila@tut.by>
2023-07-01 04:31:05 +0300

    ver 2.0

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -1,5 +1,6 @@
 package web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,12 +12,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import web.model.Car;
+import web.service.CarService;
+import web.service.CarServiceImpl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("web")
 public class WebConfig implements WebMvcConfigurer {
-
     private final ApplicationContext applicationContext;
 
     public WebConfig(ApplicationContext applicationContext) {
@@ -39,6 +46,17 @@ public class WebConfig implements WebMvcConfigurer {
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+
+    @Bean
+    public List<Car> carsList() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("VIN111", "Toyota Corolla", 2020));
+        cars.add(new Car("VIN222", "Honda Civic", 2019));
+        cars.add(new Car("VIN333", "Ford Mustang", 2022));
+        cars.add(new Car("VIN444", "Chevrolet Camaro", 2021));
+        cars.add(new Car("VIN555", "BMW X5", 2023));
+        return cars;
     }
 
 
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,31 +1,34 @@
 package web.controller;
 
-import org.springframework.lang.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarService;
-import web.service.CarServiceImpl;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
 @Controller
 public class CarController {
+
+	private static List<Car> carsList;
+	@Autowired
+	public CarController(List<Car> cars) {
+		carsList = cars;
+	}
 
 	@GetMapping(value = "/cars")
 	public static String printCars(@RequestParam(value = "count", required = false) Integer param, ModelMap model) {
 		Optional <Integer> value = Optional.ofNullable(param);
+
 		if (value.isPresent()) {
-			if (value.get() < 5) {
-				model.addAttribute("carsList", new CarServiceImpl().getCars().subList(0, value.get()));
-			}
+				model.addAttribute("carsList", carsList.subList(0, value.get()));
 		} else {
-			model.addAttribute("carsList", new CarServiceImpl().getCars());
+			model.addAttribute("carsList", carsList);
 		}
 		return "cars";
 	}
